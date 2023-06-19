49f9f0feaa6dd26bf9522c1bb8c2d022a9aeff14
tmpITK <torok.mark.patrik@gmail.com>
2023-06-19 17:49:12 +0200

    Add basic sanity check that factory works

--- scripts/lesson-3/deploy.py
+++ scripts/lesson-3/deploy.py
@@ -1,9 +1,25 @@
-from brownie import Contract, BoxFactory
+from brownie import BoxFactory
 
 from scripts.utils import get_account
 
+REDEPLOY = False
+
+
 def main():
     account = get_account()
-    print(f"Deploying BoxFactory with account {account.address}")
-    boxFactory = BoxFactory.deploy({"from": account})
-    print(f"BoxFactory deployed to {boxFactory.address}")
+    if REDEPLOY:
+        print(f"Deploying BoxFactory with account {account.address}")
+        boxFactory = BoxFactory.deploy({"from": account})
+        print(f"BoxFactory deployed to {boxFactory.address}")
+    else:
+        boxFactory = BoxFactory[-1]
+
+    print(f"Calling createBox with account {account.address}")
+    tx = boxFactory.createBox({"from": account})
+    tx.wait(1)
+
+    print(f"Calling store with account {account.address}")
+    tx = boxFactory.store(0, 42, {"from": account})
+    tx.wait(-1)
+
+    print(f"Value of box 0 is {boxFactory.retrieve(0)}")
